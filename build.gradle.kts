008d5f4238b1ccc4d7323acf2afe7265e6f3cbeb
bryansills <bryan@bryansills.com>
2024-11-20 19:40:25 -0600

    modules!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.sqldelight) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,20 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = "SqlDelightPlayground"
+
+include(":jvm-app")
